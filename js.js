d381dbf8410712c90945cae45d8105d48d7f8759
Kyle <kyleaitken24@gmail.com>
2023-01-01 14:07:28 -0500

    added prompt for user input to determine box size

--- js.js
+++ js.js
@@ -1,8 +1,27 @@
 const container = document.querySelector('.container');
 
-// create 16 boxes/squares
-for (let i = 0; i < 16 * 16; i++) {
-    const square = document.createElement('div');
-    square.classList.add('square');
-    container.appendChild(square);
+function createGrid(size) {
+    for (let i = 0; i < size * size; i++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        container.appendChild(square);
+    }
 }
+
+var size;
+do {
+    size = prompt("Enter a grid size between 4-100 (ie. number of squares per side)");
+} while (size < 4 || size > 100);
+
+createGrid(size);
+document.getElementById("gridContainer").style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+
+const boxes = document.querySelectorAll('.square');
+
+boxes.forEach((box) => {
+    box.addEventListener('mouseover', () => {
+        console.log("hovering");
+        box.style.cssText = 'background-color: lightblue';
+    });
+});
+
