facc96ac64e914199c95fb5a6b788e6e61397cbe
Kyle <kyleaitken24@gmail.com>
2023-01-01 13:17:33 -0500

    Created 16x16 grid

--- js.js
+++ js.js
@@ -1,7 +1,7 @@
 const container = document.querySelector('.container');
 
 // create 16 boxes/squares
-for (int i=0; i < 16; i++){
+for (let i = 0; i < 16 * 16; i++) {
     const square = document.createElement('div');
     square.classList.add('square');
     container.appendChild(square);
