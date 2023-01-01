d227ec30aa863fac6b3243535847cf5dfbf8ec47
Kyle <kyleaitken24@gmail.com>
2023-01-01 18:31:35 -0500

    Added documentation

--- js.js
+++ js.js
@@ -1,9 +1,20 @@
+// This script creates a grid/canvas to act as a sketch pad in response to mouse-over
+
 const buttons = document.querySelectorAll('.button-container');
+const container = document.querySelector('.container');
+
+
+// generateColor() generates a random color as a hex value equivalent
 
 function generateColor() {
     const randColor = Math.floor(Math.random()*16777215).toString(16);
     return randColor;
 }
+
+
+// Adding click listeners to the new-grid and colorful-mode buttons. 
+//   Clears previous grid, prompts user for input re: grid size, and 
+//   enables shading of squares in grid
 
 buttons.forEach((button) => {
     button.addEventListener('click', (e) => {
@@ -23,7 +34,8 @@ buttons.forEach((button) => {
     });
 });
 
-const container = document.querySelector('.container');
+
+// createGrid(size) creates a new grid of the specified size
 
 function createGrid(size) {
     for (let i = 0; i < size * size; i++) {
@@ -35,11 +47,17 @@ function createGrid(size) {
 }
 
 
+// clear() clears the grid of previous squares
+
 function clear() {
     while(container.firstChild){
         container.removeChild(container.firstChild);
     };
 }
+
+
+// getSize() prompts the user for grid size
+//    requires: user input must be an integer between 4 and 100
 
 function getSize() {
     var size;
@@ -49,14 +67,21 @@ function getSize() {
     createGrid(size);
 }
 
+
+// shade() creates a listener on each square of the grid and changes
+//    color when mouseover occurs. Color only changes once. 
+
 function shade(){
     const squares = document.querySelectorAll('#square');
     squares.forEach((sqr) => {
         sqr.addEventListener('mouseover', () => {
-            sqr.style.cssText = 'background-color: lightblue';
+            sqr.style.cssText = 'background-color: black';
         });
     });
 };
+
+
+// colorGraph(newCol) shades the graph using the randomly generated color 
 
 function colorGraph(newCol) {
     const squares = document.querySelectorAll('#square');
